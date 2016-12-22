299961fd7082f1ddd4c98cb1da5ee9e4e167611b
Liam Goodacre <goodacre.liam@gmail.com>
2016-12-22 20:58:33 +0000

    Don't alias modules for export

--- src/Type/Prelude.purs
+++ src/Type/Prelude.purs
@@ -1,11 +1,11 @@
 module Type.Prelude
-  ( module Equality
-  , module Data.Ordering
-  , module Proxy
+  ( module Type.Equality
+  , module Type.Data.Ordering
+  , module Type.Proxy
   , module Data.Symbol
   ) where
 
-import Type.Equality (class TypeEquals, from, to) as Equality
-import Type.Data.Ordering (kind Ordering, LT, EQ, GT, OProxy(..), class IsOrdering, reflectOrdering, reifyOrdering) as Data.Ordering
-import Type.Proxy (Proxy(..)) as Proxy
-import Data.Symbol (SProxy(..), class IsSymbol, reflectSymbol, reifySymbol) as Data.Symbol
+import Type.Equality (class TypeEquals, from, to)
+import Type.Data.Ordering (kind Ordering, LT, EQ, GT, OProxy(..), class IsOrdering, reflectOrdering, reifyOrdering)
+import Type.Proxy (Proxy(..))
+import Data.Symbol (SProxy(..), class IsSymbol, reflectSymbol, reifySymbol)
