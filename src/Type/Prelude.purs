e4e52cea72990c6ad778061b0f568122e6023fc2
Liam Goodacre <goodacre.liam@gmail.com>
2016-12-23 03:51:25 +0000

    Add Type.Data.Ordering; Reexport Data.Symbols, Type.Equality (#1)
    * Add Type.Data.Ordering; Reexport Data.Symbols, Type.Equality
    
    * Don't alias modules for export
    
    * Remove unnecessary coersion in reifyOrdering

--- src/Type/Prelude.purs
+++ src/Type/Prelude.purs
@@ -1,5 +1,11 @@
 module Type.Prelude
-  ( module Proxy
+  ( module Type.Equality
+  , module Type.Data.Ordering
+  , module Type.Proxy
+  , module Data.Symbol
   ) where
 
-import Type.Proxy (Proxy(..)) as Proxy
+import Type.Equality (class TypeEquals, from, to)
+import Type.Data.Ordering (kind Ordering, LT, EQ, GT, OProxy(..), class IsOrdering, reflectOrdering, reifyOrdering)
+import Type.Proxy (Proxy(..))
+import Data.Symbol (SProxy(..), class IsSymbol, reflectSymbol, reifySymbol)
