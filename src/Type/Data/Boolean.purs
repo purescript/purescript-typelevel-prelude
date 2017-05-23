ccd2236fe7f078a195524bdaaeee4709c8cfc2db
Liam Goodacre <goodacre.liam@gmail.com>
2017-05-23 19:46:52 +0100

    Add typelevel boolean, and Equals for symbol & ordering

---
+++ src/Type/Data/Boolean.purs
@@ -0,0 +1,66 @@
+module Type.Data.Boolean
+  ( kind Boolean
+  , True
+  , False
+  , BProxy(..)
+  , class IsBoolean
+  , reflectBoolean
+  , reifyBoolean
+  , class And
+  , and
+  , class Or
+  , or
+  , class Not
+  , not
+  ) where
+
+foreign import kind Boolean
+foreign import data True :: Boolean
+foreign import data False :: Boolean
+
+-- | Value proxy for `Boolean` types
+data BProxy (bool :: Boolean) = BProxy
+
+-- | Class for reflecting a type level `Boolean` at the value level
+class IsBoolean (bool :: Boolean) where
+  reflectBoolean :: BProxy bool -> Boolean
+
+instance isBooleanTrue :: IsBoolean True where reflectBoolean _ = true
+instance isBooleanFalse :: IsBoolean False where reflectBoolean _ = false
+
+-- | Use a value level `Boolean` as a type-level `Boolean`
+reifyBoolean :: forall r. Boolean -> (forall o. IsBoolean o => BProxy o -> r) -> r
+reifyBoolean true f = f (BProxy :: BProxy True)
+reifyBoolean false f = f (BProxy :: BProxy False)
+
+-- | And two `Boolean` types together
+class And (lhs :: Boolean)
+          (rhs :: Boolean)
+          (output :: Boolean) |
+          lhs -> rhs output
+instance andTrue :: And True rhs rhs
+instance andFalse :: And False rhs False
+
+and :: forall l r o. BProxy l -> BProxy r -> BProxy o
+and _ _ = BProxy
+
+-- | Or two `Boolean` types together
+class Or (lhs :: Boolean)
+         (rhs :: Boolean)
+         (output :: Boolean) |
+         lhs -> rhs output
+instance orTrue :: Or True rhs True
+instance orFalse :: Or False rhs rhs
+
+or :: forall l r o. Or l r o => BProxy l -> BProxy r -> BProxy o
+or _ _ = BProxy
+
+-- | Not a `Boolean`
+class Not (bool :: Boolean)
+          (output :: Boolean) |
+          bool -> output
+instance notTrue :: Not True False
+instance notFalse :: Not False True
+
+not :: forall i o. Not i o => BProxy i -> BProxy o
+not _ = BProxy
