639e89cf2d63a173fcc9852df997e71a5b1258dd
Liam Goodacre <goodacre.liam@gmail.com>
2017-06-16 22:34:23 +0100

    Add RowToList class

--- src/Type/Row.purs
+++ src/Type/Row.purs
@@ -1,6 +1,10 @@
 module Type.Row
   ( class RowLacks
   , class RowLacking
+  , kind RowList
+  , Nil
+  , Cons
+  , class RowToList
   ) where
 
 -- Must not be exported
@@ -32,3 +36,16 @@ instance rowLacks
      , RowLacking Entry key typ row )
   => RowLacks key row
 
+
+-- | A type-level list representation of a row
+foreign import kind RowList
+foreign import data Nil :: RowList
+foreign import data Cons :: Symbol -> Type -> RowList -> RowList
+
+-- | Extract the collection of entries in a closed row of types.
+-- | The list of entries is sorted by label and preserves duplicates.
+-- | Solved by the compiler.
+class RowToList (row :: # Type)
+                (list :: RowList) |
+                row -> list,
+                list -> row
