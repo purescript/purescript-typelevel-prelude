254a471e0dda292f7fda85bf31773a49af94ed8f
Csongor Kiss <csongor@habito.com>
2017-08-25 17:07:57 +0100

    Add type-level list of Symbols and implement insertion sort

--- src/Type/Prelude.purs
+++ src/Type/Prelude.purs
@@ -10,7 +10,7 @@ module Type.Prelude
 import Type.Data.Boolean (kind Boolean, True, False, BProxy(..), class IsBoolean, reflectBoolean, reifyBoolean)
 import Type.Data.Ordering (kind Ordering, LT, EQ, GT, OProxy(..), class IsOrdering, reflectOrdering, reifyOrdering)
 import Type.Proxy (Proxy(..))
-import Type.Data.Symbol (SProxy(..), class IsSymbol, reflectSymbol, reifySymbol, class CompareSymbol, compareSymbol, class AppendSymbol, appendSymbol)
+import Type.Data.Symbol (SProxy(..), class IsSymbol, reflectSymbol, reifySymbol, class CompareSymbol, compareSymbol, class AppendSymbol, appendSymbol, kind SList, SListProxy(..), SNil, SCons, class Sort, sort)
 import Type.Equality (class TypeEquals, from, to)
 import Type.Row (class RowLacks, class RowToList, class ListToRow, RProxy(..), RLProxy(..))
 
