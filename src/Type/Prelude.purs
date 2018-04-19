d6e0728a127e9e141db118a7ef56ae38809b16d9
Liam Goodacre <goodacre.liam@gmail.com>
2018-04-20 00:06:59 +0100

    Use/reexport solved type classes of Prim

--- src/Type/Prelude.purs
+++ src/Type/Prelude.purs
@@ -10,7 +10,7 @@ module Type.Prelude
 import Type.Data.Boolean (kind Boolean, True, False, BProxy(..), class IsBoolean, reflectBoolean, reifyBoolean)
 import Type.Data.Ordering (kind Ordering, LT, EQ, GT, OProxy(..), class IsOrdering, reflectOrdering, reifyOrdering)
 import Type.Proxy (Proxy(..))
-import Type.Data.Symbol (SProxy(..), class IsSymbol, reflectSymbol, reifySymbol, class CompareSymbol, compareSymbol, class AppendSymbol, appendSymbol)
+import Type.Data.Symbol (SProxy(..), class IsSymbol, reflectSymbol, reifySymbol, class Compare, compare, class Append, append, class Cons, uncons)
 import Type.Equality (class TypeEquals, from, to)
-import Type.Row (class RowLacks, class RowToList, class ListToRow, RProxy(..), RLProxy(..))
+import Type.Row (class Lacks, class RowToList, class ListToRow, RProxy(..), RLProxy(..))
 
--- src/Type/Row.purs
+++ src/Type/Row.purs
@@ -1,12 +1,8 @@
 module Type.Row
   ( RProxy(..)
-  , class RowLacks
-  , class RowLacking
-  , kind RowList
-  , Nil
-  , Cons
+  , module Prim.Row
+  , module Prim.RowList
   , RLProxy(..)
-  , class RowToList
   , class ListToRow
   , class RowListRemove
   , class RowListSet
@@ -16,57 +12,15 @@ module Type.Row
   , type (+)
   ) where
 
-import Prim.Row (class Cons, class Union)
+import Prim.Row (class Lacks, class Nub, class Cons)
+import Prim.RowList (kind RowList, Cons, Nil, class RowToList)
 import Type.Equality (class TypeEquals)
 import Type.Data.Symbol as Symbol
 import Type.Data.Boolean as Boolean
 
 data RProxy (row :: # Type) = RProxy
 
--- Must not be exported
-foreign import data Entry :: Type
-
--- | If you get "No type class instance was found" for this class, then your
--- | `row` shouldn't contain the label `key`.
-class RowLacking (entry :: Type)
-                 (key :: Symbol)
-                 (typ :: Type)
-                 (row :: # Type) |
-                 entry typ -> key row
-
-instance rowLacking :: RowLacking entry key entry row
-
--- | Encodes the constraint that a given row does not contain a specific key.
-class RowLacks (key :: Symbol)
-               (row :: # Type)
-
--- Append `Entry` at label `key` to the right of `row` then lookup `key` on the
--- left - if we check via instance solving that the `typ` we get back is
--- `Entry`, then `row` lacks `key`.  In the case that `row` doesn't lack
--- `key`, we get a "No type class instance found" error for:
--- `RowLacking Entry key typ row`.
-instance rowLacks
-  :: ( Cons key Entry () keyEntry
-     , Union row keyEntry rowKeyEntry
-     , Cons key typ ignored rowKeyEntry
-     , RowLacking Entry key typ row )
-  => RowLacks key row
-
-
--- | A type-level list representation of a row
-foreign import kind RowList
-foreign import data Nil :: RowList
-foreign import data Cons :: Symbol -> Type -> RowList -> RowList
-
 data RLProxy (rowList :: RowList) = RLProxy
-
--- | Extract the collection of entries in a closed row of types.
--- | The list of entries is sorted by label and preserves duplicates.
--- | The inverse of this operation is `ListToRow`.
--- | Solved by the compiler.
-class RowToList (row :: # Type)
-                (list :: RowList) |
-                row -> list
 
 -- | Convert a RowList to a row of types.
 -- | The inverse of this operation is `RowToList`.
