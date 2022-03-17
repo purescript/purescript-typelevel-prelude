1714d366a44c36fabb9a5f96048311619d2c83e3
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-17 08:56:46 -0500

    Add type-level op for FLIP

--- src/Type/Function.purs
+++ src/Type/Function.purs
@@ -15,14 +15,9 @@ infixr 0 type APPLY as $
 -- |
 -- | For example...
 -- | ```
--- | FLIP Int Maybe == Maybe Int
+-- | FLIP Int Maybe == Int # Maybe == Maybe Int
 -- | ```
--- | Note: an infix for FLIP (e.g. `Int # Maybe`) is not allowed.
--- | Before the `0.14.0` release, we used `# Type` to refer to a row of types.
--- | In  the `0.14.0` release, the `# Type` syntax was deprecated,
--- | and `Row Type` is the correct way to do this now. To help mitigate
--- | breakage, `# Type` was made an alias to `Row Type`. When the `# Type`
--- | syntax is fully dropped in a later language release, we can then
--- | support the infix version: `Int # Maybe`.
 type FLIP :: forall a b. a -> (a -> b) -> b
 type FLIP a f = f a
+
+infixl 1 type FLIP as #
